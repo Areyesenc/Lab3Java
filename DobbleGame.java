3c0d9fb73a20807a6d31e2e43fc8dbcdcccbef6c
Areyesenc <55030875+Areyesenc@users.noreply.github.com>
2022-06-17 02:03:36 -0400

    Create DobbleGame.java

---
+++ DobbleGame.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class DobbleGame {
+  
+   public DobbleGame() {
+        dobble = null;
+        players = new ArrayList<>();
+        input = new Scanner(System.in);
+    }
