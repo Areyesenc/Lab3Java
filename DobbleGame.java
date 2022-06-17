820180506daa1156623c71aa6bc40d7432a2e7ca
Areyesenc <55030875+Areyesenc@users.noreply.github.com>
2022-06-17 03:27:22 -0400

    Update DobbleGame.java

--- DobbleGame.java
+++ DobbleGame.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//Main.java
+//public static void main(String[] args) {
+
+}
 
 public class DobbleGame {
   
    public DobbleGame() {
-        dobble = null;
+        dobble = null; // jugadores. (play)
         players = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-    }
+        int option;
   
 //playround para iniciar el juego.  
   
@@ -28,6 +32,60 @@ public class DobbleGame {
         System.out.println("2. Registrar jugador");
         System.out.println("3. Jugar!");
         System.out.println("4. Visualizar estado completo del juego");
+        system.out.println("5. Salir");
         //syso ("INTRODUZCA SU OPCION" + algo);
   }
   
+// 
+
+        while (option != 5) {
+            option = Menu();
+            switch (option) {
+                case 1:
+                    createGame();
+                    break;
+                case 2:
+                    register();
+                    break;
+                case 3:
+                    play();
+                    break;
+                case 4:
+                    status();
+                    break;
+                case 5:
+                    System.out.println("Gracias!");
+                    break;
+                default:
+                    System.out.println("Opción invalida");
+            }
+        }
+    }
+
+  //facil
+void createGame(){
+  
+}
+  //facil .. --addP(new player(name))--  2 jugadores como mínimo .. 
+void register(){
+    
+}
+  
+  //(ver la hoja)
+void playMenu(){
+    if (players.size() < 2){
+    System.out.println("debes añadir al menos dos jugadores"
+    return;
+  }
+  if (dobble == null) { //esto indica que si no se ha creado un juego, regresa al menu para crearlo!
+    System.out.println("crea un juego");
+    return;
+    
+  //si esta correcto todo, entonces aquí ya se podría jugar y ver status.. 
+}
+  
+                       
+//quien gana y quien pierde... debería ir dentro del metodo toString() cómo representación del estado del juego (?)
+void status(){
+    
+}
