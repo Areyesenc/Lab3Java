676fcbb91be7205a9ede4bb7694d0daf3a6f156e
Areyesenc <55030875+Areyesenc@users.noreply.github.com>
2022-06-17 07:43:10 -0400

    Update player.java

--- player.java
+++ player.java
@@ -1,3 +1,14 @@
-public class Player {
+package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private final String name;
+    private List<Card> cards;
+
+    public Player(String name) {
+        this.name = name;
+        cards = new ArrayList<>();
     }
+}
