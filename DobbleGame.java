e77500cc00a66674363a425a4b8c745514fb7b25
Areyesenc <55030875+Areyesenc@users.noreply.github.com>
2022-06-17 02:27:35 -0400

    Update DobbleGame.java

--- DobbleGame.java
+++ DobbleGame.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class DobbleGame {
   
    public DobbleGame() {
         dobble = null;
         players = new ArrayList<>();
-        input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
     }
+  
+//playround para iniciar el juego.  
+  
+   public void playRound() {
+     
+   }
+  
+  //para clase "play" usa idea "switch - case(del 1 al 4) - defatul (por si menu no existe) ...
+  
+  //idea del menu como sería...
+  int Menu(){
+        System.out.println();
+        System.out.println();
+        System.out.println("Bienvenido al juego de Dobble!");
+        System.out.println();
+        System.out.println("1. Crear un juego");
+        System.out.println("2. Registrar jugador");
+        System.out.println("3. Jugar!");
+        System.out.println("4. Visualizar estado completo del juego");
+        //syso ("INTRODUZCA SU OPCION" + algo);
+  }
+  
